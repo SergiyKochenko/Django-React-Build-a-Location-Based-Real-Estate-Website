548870dad0e362b9f8fa26ca1648544c8d0f088e
Sergiy <sergiykochenko@gmail.com>
2025-03-16 23:52:26 +0000

    Getting started with React fontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App () {
+  return (
+    <div>
+      <h1>This is an H1</h1>
+      <p>
+        This is a paragraph
+      </p>
+    </div>
+  )
+}
+
+export default App
