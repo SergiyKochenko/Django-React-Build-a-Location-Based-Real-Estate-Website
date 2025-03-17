c5b0dcd37bdda24363365f2d289dea31992bdf70
Sergiy <sergiykochenko@gmail.com>
2025-03-17 15:02:06 +0000

    Testing React Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,10 @@ import logo from './logo.svg'
 import './App.css'
 import SecondComponent from './Components/SecondComponent'
 import React from 'react'
+import AppleComponent from './Components/AppleComponent'
 
 function App () {
-  function AppleDisplay (n) {
-    if (n === 0 || n === 1) {
-      return `Sergiy has ${n} apple`
-    } else if (n > 1) {
-      return `Sergiy has ${n} apples`
-    } else {
-      return `Sergiy owes us ${Math.abs(n)} apples`
-    }
-  }
-  return (
-    <div>
-      <h1>{AppleDisplay(-2)}</h1>
-    </div>
-  )
+  return <AppleComponent />
 }
 
 export default App
