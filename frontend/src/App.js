aa3913c92b612d07921040409943b9afc8853a75
Sergiy <sergiykochenko@gmail.com>
2025-03-17 15:35:47 +0000

    Testing React Propertis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,14 @@ import './App.css'
 import SecondComponent from './Components/SecondComponent'
 import React from 'react'
 import AppleComponent from './Components/AppleComponent'
+import Fruit from './Components/Fruit'
 
 function App () {
-  return <AppleComponent />
+  return(
+    <>
+    <Fruit name='banana' color='yellow' />
+    <Fruit name='strawberry' color='red' />
+    </>)
 }
 
 export default App
