97039382fbe95ca8e6553d08746bcc7615366688
Sergiy <sergiykochenko@gmail.com>
2025-03-17 01:21:07 +0000

    Added React Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg'
 import './App.css'
+import SecondComponent from './Components/SecondComponent'
 
 function App () {
+  function MyComponent () {
+    return <h1>This is My First Component</h1>
+  }
   return (
     <div>
-      <h1>This is an H1</h1>
-      <p>
-        This is a paragraph
-      </p>
+      <h1>This is the app component</h1>
+      <MyComponent />
+      <SecondComponent />
+      <SecondComponent />
+      <SecondComponent />
+      <SecondComponent />
+      <SecondComponent />
     </div>
   )
 }
