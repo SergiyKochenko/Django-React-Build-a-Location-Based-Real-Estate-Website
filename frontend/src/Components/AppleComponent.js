f0c1d817dcea657cfe5f13734b6c7da0e7f2ba59
Sergiy <sergiykochenko@gmail.com>
2025-03-18 19:27:15 +0000

    Installed react-router-dom and Added Navigation to pages Home, Login & Listings

--- frontend/src/Components/AppleComponent.js
+++ frontend/src/Components/AppleComponent.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function AppleComponent () {
-  const [numberOfApples, setNumberOfApples] = useState(1 + 3)
+  const [numberOfApples, setNumberOfApples] = useState(1 + 2)
 
   function AppleDisplay (numberOfApples) {
     if (numberOfApples === 0 || numberOfApples === 1) {
@@ -18,13 +18,37 @@ function AppleComponent () {
   function decreaseApples(){
     setNumberOfApples((currentValue) => currentValue -1)
   }
+
+  function TooManyApplesDisplay () {
+    if (numberOfApples > 10){
+    return <h1>Sergiy has too many apples</h1>
+    }else{
+      return <h1>Sergiy has a reasonable amount of apples</h1>
+    }
+  }
   return (
     <>
     <div>
       <h1>{AppleDisplay(numberOfApples)}</h1>
     </div>
     <button onClick={increaseApples} className='add-btn'>Increase</button>
-    <button onClick={decreaseApples} className='decrease-btn'>Decrease</button>
+    <button 
+      style={{display: numberOfApples <= 0 ? 'none' : ''}} 
+      onClick={decreaseApples} 
+      className='decrease-btn'
+      >
+        Decrease
+        </button>
+
+
+
+{
+  /* {TooManyApplesDisplay()}  */
+}
+
+
+{numberOfApples > 10 ? <h1>Sergiy has too many apples</h1> : <h1>Sergiy has a reasonable amount of apples</h1>}
+
     </>
   )
 }
--- frontend/src/Components/SecondComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function SecondComponent () {
-  return <h1>This is My Second Component</h1>
-}
-
-export default SecondComponent
