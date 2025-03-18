93faf45f15ed4e000741620dc20b565acceba7ad
Sergiy <sergiykochenko@gmail.com>
2025-03-18 19:40:29 +0000

    Added Link to the Home page to Login & Listings

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home () {
   return (
     <div>
       <h1>This is the home page</h1>
+      <br />
+      <Link to='/login'> Login
+      </Link>
+      <br />
+      <Link to='/listings'> Listings
+      </Link>
     </div>
   )
 }
