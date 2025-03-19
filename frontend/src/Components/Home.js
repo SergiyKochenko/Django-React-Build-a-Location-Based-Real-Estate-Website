6a8757df0c1ea5f057ccf04f9b3b9de006be12b1
Sergiy <sergiykochenko@gmail.com>
2025-03-19 22:48:21 +0000

    Building the home page: Haeder

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,21 +1,42 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, Grid, AppBar, Toolbar } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 
+import CustomCard from './CustomCard';
+import CssBaseline from '@mui/material/CssBaseline';
+
 const useStyles = makeStyles({
- divStyle: {
-   width: '50%',
-   border: "2px solid red",
-   padding: '15px'
- },
-  btnStyle: {
-    backgroundColor: 'red',
-    color: 'white',
-    '&:hover': {
-      backgroundColor: 'green'
-    }
-  }
+  leftNav: {
+    marginRight: 'auto',
+
+  },
+  rightNav: {
+    marginLeft: 'auto',
+    marginRight: '10rem'
+
+  },
+propertyBtn: {
+  background: 'green',
+  color: 'white',
+  width: '15rem',
+  fontSize: '1.1rem',
+  marginRight: '1rem',
+  '&:hover': {
+    backgroundColor: 'blue',
+  },
+},
+loginBtn: {
+  background: 'white',
+  color: 'black',
+  width: '15rem',
+  fontSize: '1.1rem',
+  marginLeft: '1rem',
+  '&:hover': {
+    backgroundColor: 'green',
+},
+}
+ 
 })
 
 function Home () {
@@ -23,21 +44,22 @@ function Home () {
   const classes = useStyles();
   return (
     <>
-    <div className={classes.divStyle}>
-    <Typography variant='h4'>This is the title</Typography>
-    <Typography variant='body1'>
-      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-      </Typography>
-    <Button
-      onClick={() => setBtnColor('success')}
-      variant='contained'
-      size='medium'
-      className={classes.btnStyle}>
-      GO TO LISTINGS
-    </Button>
-
-    </div>
-
+    <CssBaseline />
+     <AppBar position="static" style={{ backgroundColor: 'black' }}>
+        <Toolbar>
+          <div className={classes.leftNav}>
+            <Button color="inherit"><Typography variant='h4'>LBREP</Typography></Button>
+          </div>
+          <div>
+          <Button color="inherit" style={{marginRight: '2rem'}}><Typography variant='h6'>Listings</Typography></Button>
+          <Button color="inherit" style={{marginLeft: '2rem'}}><Typography variant='h6'>Agencies</Typography></Button>
+          </div>
+          <div className={classes.rightNav}>
+            <Button className={classes.propertyBtn}>Add Property</Button>
+            <Button className={classes.loginBtn}>Login</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
