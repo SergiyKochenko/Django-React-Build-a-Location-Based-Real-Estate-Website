3d704bf5d9f64756c3571f8154b595e1da2fe987
Sergiy <sergiykochenko@gmail.com>
2025-03-20 00:53:15 +0000

    Added Assets, created the rest of the home page and the Header, re-organized the code

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,42 +1,50 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+// Material UI
 import { Button, Typography, Grid, AppBar, Toolbar } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 
+// Components
 import CustomCard from './CustomCard';
-import CssBaseline from '@mui/material/CssBaseline';
+import Header from './Header';
+
+// Assets
+import city from './Assets/city.jpg'
+
 
 const useStyles = makeStyles({
-  leftNav: {
-    marginRight: 'auto',
+
+cityImg: {
+  width: '100%',
+  height: '92vh',
+  objectFit: 'cover',
+  position: 'absolute',
+  zIndex: '-1'
+},
+
+overlayText: {
+  position: 'absolute',
+  zIndex: '100',
+  top: '100px',
+  left: '20px',
+  textAlign: 'center',
+},
+homeText: {
+  color: 'gold',
+  fontWeight: 'bolder',
+},
+homeBtn: {
+  fontSize: '3.5rem',
+  borderRadius: '15px',
+  backgroundColor: 'green',
+  marginTop: '2rem',
+  boxShadow: '3px 3px 3px white',
 
   },
-  rightNav: {
-    marginLeft: 'auto',
-    marginRight: '10rem'
 
-  },
-propertyBtn: {
-  background: 'green',
-  color: 'white',
-  width: '15rem',
-  fontSize: '1.1rem',
-  marginRight: '1rem',
-  '&:hover': {
-    backgroundColor: 'blue',
-  },
-},
-loginBtn: {
-  background: 'white',
-  color: 'black',
-  width: '15rem',
-  fontSize: '1.1rem',
-  marginLeft: '1rem',
-  '&:hover': {
-    backgroundColor: 'green',
-},
-}
- 
+
+
 })
 
 function Home () {
@@ -44,22 +52,14 @@ function Home () {
   const classes = useStyles();
   return (
     <>
-    <CssBaseline />
-     <AppBar position="static" style={{ backgroundColor: 'black' }}>
-        <Toolbar>
-          <div className={classes.leftNav}>
-            <Button color="inherit"><Typography variant='h4'>LBREP</Typography></Button>
-          </div>
-          <div>
-          <Button color="inherit" style={{marginRight: '2rem'}}><Typography variant='h6'>Listings</Typography></Button>
-          <Button color="inherit" style={{marginLeft: '2rem'}}><Typography variant='h6'>Agencies</Typography></Button>
-          </div>
-          <div className={classes.rightNav}>
-            <Button className={classes.propertyBtn}>Add Property</Button>
-            <Button className={classes.loginBtn}>Login</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+
+      <div style={{position: 'relative'}}>
+      <img src={city} className={classes.cityImg} alt="City view"/>
+      <div className={classes.overlayText}>
+        <Typography variant='h1' className={classes.homeText}>FIND YOUR <span style={{color: "blue"}}>PROPERTY</span> ON THE LBREP WEBSITE</Typography>
+        <Button variant='contained' className={classes.homeBtn}>SEE ALL PROPERTIES</Button>
+      </div>
+      </div>
     </>
   )
 }
