a404e2e5ff3ff548de24f721d37a0046c7a7389d
Sergiy <sergiykochenko@gmail.com>
2025-03-23 09:23:33 +0000

    Setting Up Django Cors Headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,12 +52,14 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
+    'corsheaders',
 
     'listings.apps.ListingsConfig',
     'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,3 +147,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
