3cc2feb424f6b9a2aa6df42a66648cf39338fc15
Sergiy <sergiykochenko@gmail.com>
2025-03-23 10:44:42 +0000

    Testing the useEfect hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,12 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 // Components
-import Home from './Components/Home'
-import Login from './Components/Login'
+import Home from './Components/Home';
+import Login from './Components/Login';
 import Listings from './Components/Listings'
 import Header from './Components/Header';
+import Testing from './Components/Testing';
+
 
 function App () {
   return (
@@ -21,6 +23,7 @@ function App () {
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/listings' element={<Listings/>} />
+        <Route path='/testing' element={<Testing/>} />
       </Routes>
     </BrowserRouter>
     </StyledEngineProvider>
