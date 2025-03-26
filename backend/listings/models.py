59fcf26b58b881d4028e80e3f95d8ca1303cadb2
Sergiy <sergiykochenko@gmail.com>
2025-03-26 23:45:37 +0000

    Added, - Placing the map on the form

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -1,21 +1,22 @@
 from random import choices
+from django.contrib.gis.geos import Point
 from django.contrib.gis.db import models
 from django.utils import timezone
-from django.contrib.gis.geos import Point
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
     
 
 class Listing(models.Model):
-    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
     choices_area = (
-        ('inner London', 'Inner London'),
-        ('outer London', 'Outer London'),
+        ('Inner London', 'Inner London'),
+        ('Outer London', 'Outer London'),
     )
-    area = models.CharField(max_length=20, blank=True, 
+    area = models.CharField(max_length=20, blank=True,
                             null=True, choices=choices_area)
     borough = models.CharField(max_length=50, blank=True, null=True)
     choices_listing_type = (
@@ -33,8 +34,8 @@ class Listing(models.Model):
         max_length=20, blank=True, null=True, choices=choices_property_status)
     price = models.DecimalField(max_digits=50, decimal_places=0)
     choices_rental_frequency = (
-        ('Monthly', 'Monthly'),
-        ('Weekly', 'Weekly'),
+        ('Month', 'Month'),
+        ('Week', 'Week'),
         ('Day', 'Day'),
     )
     rental_frequency = models.CharField(
@@ -48,12 +49,18 @@ class Listing(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
-    picture1 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
-    picture2 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
-    picture3 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
-    picture4 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
-    picture5 = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/%d/')
+    picture1 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture2 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture3 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture4 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture5 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
 
     def __str__(self):
         return self.title
+
 
